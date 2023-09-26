c111b4a7d77eebc6483b47ab55875e4dda02bbe1
ysagar11y <46919299+ysagar11y@users.noreply.github.com>
2023-09-26 16:22:18 +0530

    Update EmployeeDao.java
    Adding RowMapper function

--- springjdbc/src/main/java/com/springjdbc/Dao/EmployeeDao.java
+++ springjdbc/src/main/java/com/springjdbc/Dao/EmployeeDao.java
@@ -1,5 +1,7 @@
 package com.springjdbc.Dao;
 
+import java.util.List;
+
 import com.springjdbc.Entity.Employee;
 
 public interface EmployeeDao {
@@ -8,6 +10,10 @@ public interface EmployeeDao {
 	
 	public void update();
 	
+	public void delete(int id);
+	
 	public Employee select(int id);
+	
+	public List<Employee> selectAll();
 
 }
